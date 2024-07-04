047989ce738ee83f864f9e88747ff299e9dd9483
asueeer <asueeer@163.com>
2024-07-04 19:27:19 +0800

    change import path

---
+++ deep_inc/server/server.cc
@@ -0,0 +1,10 @@
+#include "server.h"
+#include <iostream>
+
+namespace deep_inc{
+    namespace server{
+        void start_server(){
+            std::cout << "Server started" << std::endl;
+        }
+    }
+}
---
+++ deep_inc/server/server.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+namespace deep_inc {
+    namespace server {
+        void start_server();
+    }
+}
+
+#endif  // SERVER_H
