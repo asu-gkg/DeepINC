6e011c06a4050e9c25156652c77f8725bdbd76ee
asueeer <asueeer@163.com>
2024-07-04 19:41:54 +0800

    change setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,13 @@ from distutils.errors import CompileError, DistutilsError, DistutilsPlatformErro
 import subprocess
 import traceback
 import os
+import sys
+import textwrap
+import shlex
+
 
 server_lib = Extension('deep_inc.server.c_lib', [])
+extensions_to_build = [server_lib]
 
 def has_rdma_header():
     ret_code = subprocess.call(
@@ -35,10 +40,90 @@ def get_ucx_prefix():
     ucx_prefix = os.getenv('UCX_PREFIX', ucx_default_home)
     return ucx_prefix
 
+def test_compile(build_ext, name, code, libraries=None, include_dirs=None, library_dirs=None,
+                 macros=None, extra_compile_preargs=None, extra_link_preargs=None):
+    test_compile_dir = os.path.join(build_ext.build_temp, 'test_compile')
+    if not os.path.exists(test_compile_dir):
+        os.makedirs(test_compile_dir)
+
+    source_file = os.path.join(test_compile_dir, '%s.cc' % name)
+    with open(source_file, 'w') as f:
+        f.write(code)
+
+    compiler = build_ext.compiler
+    [object_file] = compiler.object_filenames([source_file])
+    shared_object_file = compiler.shared_object_filename(
+        name, output_dir=test_compile_dir)
+
+    compiler.compile([source_file], extra_preargs=extra_compile_preargs,
+                     include_dirs=include_dirs, macros=macros)
+    compiler.link_shared_object(
+        [object_file], shared_object_file, libraries=libraries, library_dirs=library_dirs,
+        extra_preargs=extra_link_preargs)
+
+    return shared_object_file
+
+
+def get_cpp_flags(build_ext):
+    last_err = None
+    default_flags = ['-std=c++11', '-fPIC', '-Ofast', '-Wall', '-march=native', '-mno-avx512f']
+    flags_to_try = []
+    if sys.platform == 'darwin':
+        # Darwin most likely will have Clang, which has libc++.
+        flags_to_try = [default_flags + ['-stdlib=libc++'],
+                        default_flags]
+    else:
+        flags_to_try = [default_flags,
+                        default_flags + ['-stdlib=libc++']]
+    for cpp_flags in flags_to_try:
+        try:
+            test_compile(build_ext, 'test_cpp_flags', extra_compile_preargs=cpp_flags,
+                         code=textwrap.dedent('''\
+                    #include <unordered_map>
+                    void test() {
+                    }
+                    '''))
+
+            return cpp_flags
+        except (CompileError, LinkError):
+            last_err = 'Unable to determine C++ compilation flags (see error above).'
+        except Exception:
+            last_err = 'Unable to determine C++ compilation flags.  ' \
+                       'Last error:\n\n%s' % traceback.format_exc()
+
+    raise DistutilsPlatformError(last_err)
+
+def get_link_flags(build_ext):
+    last_err = None
+    libtool_flags = ['-Wl,-exported_symbols_list,byteps.exp']
+    ld_flags = ['-Wl,--version-script=byteps.lds', '-fopenmp']
+    flags_to_try = []
+    if sys.platform == 'darwin':
+        flags_to_try = [libtool_flags, ld_flags]
+    else:
+        flags_to_try = [ld_flags, libtool_flags]
+    for link_flags in flags_to_try:
+        try:
+            test_compile(build_ext, 'test_link_flags', extra_link_preargs=link_flags,
+                         code=textwrap.dedent('''\
+                    void test() {
+                    }
+                    '''))
+
+            return link_flags
+        except (CompileError, LinkError):
+            last_err = 'Unable to determine C++ link flags (see error above).'
+        except Exception:
+            last_err = 'Unable to determine C++ link flags.  ' \
+                       'Last error:\n\n%s' % traceback.format_exc()
+
+    raise DistutilsPlatformError(last_err)
+
+
 def build_server(build_ext, options):
     server_lib.define_macros = options['MACROS']
     server_lib.include_dirs = options['INCLUDES']
-    server_lib.sources = ['server/server.cc']
+    server_lib.sources = ['deep_inc/server/server.cc']
     server_lib.extra_compile_args = options['COMPILE_FLAGS'] + options['EXTRA_COMPILE_FLAGS']
     server_lib.extra_link_args = options['LINK_FLAGS']
     server_lib.extra_objects = options['EXTRA_OBJECTS']
@@ -46,12 +131,47 @@ def build_server(build_ext, options):
 
     build_ext.build_extension(server_lib)
 
+def get_common_options(build_ext):
+    cpp_flags = get_cpp_flags(build_ext)
+    link_flags = get_link_flags(build_ext)
+
+    MACROS = [('EIGEN_MPL2_ONLY', 1)]
+    SOURCES = []
+    COMPILE_FLAGS = cpp_flags
+    LINK_FLAGS = link_flags
+
+    LIBRARY_DIRS = []
+    LIBRARIES = []
+
+    # RDMA and NUMA libs
+    LIBRARIES += ['numa']
+
+    # auto-detect rdma
+    if has_rdma_header():
+        LIBRARIES += ['rdmacm', 'ibverbs', 'rt']
+    if use_ucx():
+        LIBRARIES += ['ucp', 'uct', 'ucs', 'ucm']
+        ucx_home = get_ucx_home()
+        if ucx_home:
+            INCLUDES += [f'{ucx_home}/include']
+            LIBRARY_DIRS += [f'{ucx_home}/lib']
+
+    return dict(MACROS=MACROS,
+                INCLUDES=INCLUDES,
+                SOURCES=SOURCES,
+                COMPILE_FLAGS=COMPILE_FLAGS,
+                LINK_FLAGS=LINK_FLAGS,
+                LIBRARY_DIRS=LIBRARY_DIRS,
+                LIBRARIES=LIBRARIES)
+
+
 
 # run the customize_compiler
 class custom_build_ext(build_ext):
     def build_extensions(self):
+        options = get_common_options(self)
         try:
-            build_server(self)
+            build_server(self, [])
         except:
             raise DistutilsSetupError('An ERROR occured while building the server module.\n\n'
                                       '%s' % traceback.format_exc())
@@ -66,6 +186,7 @@ setup(
     cmdclass={
         'build_ext': custom_build_ext,
     },
+    ext_modules=extensions_to_build,
     entry_points={
         'console_scripts': [
             
