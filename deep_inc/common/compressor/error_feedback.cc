fee95f7806f48078c288fd14df0f72f7c902519f
asueeer <asueeer@163.com>
2024-07-07 17:32:59 +0800

    change namespace

--- deep_inc/common/compressor/error_feedback.cc
+++ deep_inc/common/compressor/error_feedback.cc
@@ -15,33 +15,39 @@
 
 #include "error_feedback.h"
 
-namespace byteps {
-namespace common {
-namespace compressor {
+namespace deep_inc
+{
+  namespace common
+  {
+    namespace compressor
+    {
 
-tensor_t ErrorFeedback::Compress(tensor_t grad) {
-  // 1. grad <- grad + error
-  UpdateGradient(grad);
+      tensor_t ErrorFeedback::Compress(tensor_t grad)
+      {
+        // 1. grad <- grad + error
+        UpdateGradient(grad);
 
-  // 2. c <- Compress(grad)
-  auto compressed = _cptr->Compress(grad);
+        // 2. c <- Compress(grad)
+        auto compressed = _cptr->Compress(grad);
 
-  // 3. e <- grad - Decompress(c)
-  UpdateError(grad, compressed);
+        // 3. e <- grad - Decompress(c)
+        UpdateError(grad, compressed);
 
-  return compressed;
-}
+        return compressed;
+      }
 
-tensor_t ErrorFeedback::Decompress(tensor_t compressed) {
-  // directly forward to internal compressor
-  return _cptr->Decompress(compressed);
-}
+      tensor_t ErrorFeedback::Decompress(tensor_t compressed)
+      {
+        // directly forward to internal compressor
+        return _cptr->Decompress(compressed);
+      }
 
-void ErrorFeedback::UpdateError(tensor_t corrected, tensor_t compressed) {
-  tensor_t error{_error.get(), _size, corrected.dtype};
-  _cptr->FastUpdateError(error, corrected, compressed);
-}
+      void ErrorFeedback::UpdateError(tensor_t corrected, tensor_t compressed)
+      {
+        tensor_t error{_error.get(), _size, corrected.dtype};
+        _cptr->FastUpdateError(error, corrected, compressed);
+      }
 
-}  // namespace compressor
-}  // namespace common
-}  // namespace byteps
+    } // namespace compressor
+  } // namespace common
+} // namespace deep_inc
