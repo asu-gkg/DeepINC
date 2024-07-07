57742f179c208c596f287f09f568ab37f3b9e979
asueeer <asueeer@163.com>
2024-07-07 17:29:01 +0800

    init compressor

--- setup.py
+++ setup.py
@@ -145,7 +145,11 @@ def get_common_options(build_ext):
                'deep_inc/common/nccl_manager.cc',
                'deep_inc/common/ready_table.cc',
                'deep_inc/common/scheduled_queue.cc',
-               'deep_inc/common/shared_memory.cc',]
+               'deep_inc/common/shared_memory.cc',] + [
+                   'deep_inc/common/compressor/error_feedback.cc',
+                   'deep_inc/common/compressor/momentum.cc',
+                   'deep_inc/common/compressor/impl/dithering.cc',
+               ]
     COMPILE_FLAGS = cpp_flags
     LINK_FLAGS = link_flags
     INCLUDES = ['ps-lite/include']
